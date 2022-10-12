daacc20b9b74a0c2348151d62d5048af684c3fc4
Fuat Suzer <a.fuatsuzer@hotmail.com>
2022-10-12 10:18:56 +0300

    first commit

---
+++ time_conv/time_conv/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static string timeConversion(string s)
+    {
+        bool twelveAM = false;
+        int hour = Convert.ToInt32(s.Substring(0, 2));
+        if (hour+12==24) twelveAM = true;
+        string newHour = s.Remove(8);
+        newHour = newHour.Remove(0,2);
+        if (s.Contains('A'))
+        {
+            if (twelveAM) return "00" + newHour;
+            else
+            {
+                if (hour<12)
+                {
+                    return "0"+Convert.ToString(hour) + newHour;
+                }
+                else return Convert.ToString(hour) + newHour;
+
+            }
+        }
+        else
+        {
+            if (twelveAM) return "12" + newHour;
+            else return Convert.ToString(hour+12) + newHour;
+        }
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        string result = Result.timeConversion("01:00:00AM");
+        Console.WriteLine(result);
+        Console.ReadKey();
+    }
+}
---
+++ time_conv/time_conv/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("time_conv")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("time_conv")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("726b0e9d-caf3-4c9f-9a97-6fa307b13eff")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
